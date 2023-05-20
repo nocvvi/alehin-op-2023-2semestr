dfeb99d9ab2a52c5b1f727b376a6b73ab8d2980e
Дмитрий <dima-vrn00@inbox.ru>
2023-05-20 17:08:17 +0300

    add 42

---
+++ CP4/less 42/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static int countNumbersIntervals(int[][] intervals) {
+        int count = 0;
+        for (int[] interval : intervals) {
+            int start = interval[0];
+            int end = interval[1];
+            count += (end - start - 1);
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        int[][] intervals = {{1, 2}, {6, 10}, {11, 15}};
+        int count = countNumbersIntervals(intervals);
+        System.out.println(count);
+    }
+}
