ae110fc4a204dc044b5cab284a5861efbc8e215f
Дмитрий <dima-vrn00@inbox.ru>
2023-05-20 18:10:02 +0300

    add 5 cp

---
+++ КТ5/Calculator/src/Calculator.java
@@ -0,0 +1,112 @@
+import java.util.Stack;
+
+public class Calculator {
+    // Метод для проверки, является ли символ оператором
+    private boolean isOperator(char c) {
+        return c == '+' || c == '-' || c == '*' || c == '/' || c == '^' || c == '|' || c == '%';
+    }
+
+    // Метод для выполнения операции над двумя числами
+    private double executeOperation(double num1, double num2, char operator) {
+        switch (operator) {
+            case '+':
+                return num1 + num2;
+            case '-':
+                return num1 - num2;
+            case '*':
+                return num1 * num2;
+            case '/':
+                return num1 / num2;
+            case '%':
+                return num1 % num2;
+            case '^':
+                return Math.pow(num1, num2);
+            default:
+                throw new IllegalArgumentException("Неверный оператор: " + operator);
+        }
+    }
+
+    // Метод для вычисления значения математического выражения
+    public double evaluateExpression(String expression) {
+        Stack<Double> numbers = new Stack<>();
+        Stack<Character> operators = new Stack<>();
+
+        for (int i = 0; i < expression.length(); i++) {
+            char currentChar = expression.charAt(i);
+
+            if (Character.isDigit(currentChar)) {
+                // Если текущий символ - цифра, извлекаем число из строки и добавляем в стек чисел
+                StringBuilder numBuilder = new StringBuilder();
+                numBuilder.append(currentChar);
+
+                // Проверяем следующие символы, чтобы извлечь полное число
+                while (i + 1 < expression.length() && Character.isDigit(expression.charAt(i + 1))) {
+                    numBuilder.append(expression.charAt(i + 1));
+                    i++;
+                }
+
+                double number = Double.parseDouble(numBuilder.toString());
+                numbers.push(number);
+            } else if (currentChar == '(') {
+                // Если текущий символ - открывающая скобка, добавляем ее в стек операторов
+                operators.push(currentChar);
+            } else if (currentChar == ')') {
+                // Если текущий символ - закрывающая скобка, выполняем операции внутри скобок
+                while (!operators.isEmpty() && operators.peek() != '(') {
+                    char operator = operators.pop();
+                    double num2 = numbers.pop();
+                    double num1 = numbers.pop();
+                    double result = executeOperation(num1, num2, operator);
+                    numbers.push(result);
+                }
+
+                // Удаляем открывающую скобку из стека операторов
+                if (!operators.isEmpty() && operators.peek() == '(') {
+                    operators.pop();
+                }
+            } else if (isOperator(currentChar)) {
+                // Если текущий символ - оператор, выполняем операции в зависимости от приоритета
+                while (!operators.isEmpty() && getOperatorPriority(operators.peek()) >= getOperatorPriority(currentChar)) {
+                    char operator = operators.pop();
+                    double num2 = numbers.pop();
+                    double num1 = numbers.pop();
+                    double result = executeOperation(num1, num2, operator);
+                    numbers.push(result);
+                }
+
+                // Добавляем текущий оператор в стек операторов
+                operators.push(currentChar);
+            }
+        }
+
+        // Выполняем оставшиеся операции
+        while (!operators.isEmpty()) {
+            char operator = operators.pop();
+            double num2 = numbers.pop();
+            double num1 = numbers.pop();
+            double result = executeOperation(num1, num2, operator);
+            numbers.push(result);
+        }
+
+        // Возвращаем конечный результат
+        return numbers.pop();
+    }
+
+    // Метод для определения приоритета оператора
+    private int getOperatorPriority(char operator) {
+        switch (operator) {
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+            case '%':
+                return 2;
+            case '^':
+            case '|':
+                return 3;
+            default:
+                return 0;
+        }
+    }
+}
---
+++ КТ5/Calculator/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        HistoryManager historyManager = new HistoryManager();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Введите математическое выражение (или 'выход' для завершения): ");
+            String input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("выход")) {
+                break;
+            }
+
+            double result = calculator.evaluateExpression(input);
+            System.out.println("Результат: " + result);
+
+            historyManager.saveExpression(input, result);
+        }
+
+        List<String> history = historyManager.loadHistory();
+        System.out.println("История вычислений:");
+        for (String entry : history) {
+            System.out.println(entry);
+        }
+    }
+}
